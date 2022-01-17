ed4855f778ebc699b2f08f9e95cc7a2aef1b2ea3
Seul <angel3030303@gmail.com>
2022-01-17 22:41:33 +0900

    second commit

--- app/src/main/java/com/example/qna/LoginActivity.java
+++ app/src/main/java/com/example/qna/LoginActivity.java
@@ -9,6 +9,8 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.toolbox.Volley;
